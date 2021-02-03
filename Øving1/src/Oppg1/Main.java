079954e00f1e43efc7b9734238d094ad9794e2fb
581515 <47955690+Yosafe581515@users.noreply.github.com>
2021-02-04 00:49:30 +0100

    Update Main.java

--- Øving1/src/Oppg1/Main.java
+++ Øving1/src/Oppg1/Main.java
@@ -2,21 +2,17 @@ package Oppg1;
 
 public class Main {
 	public static void main(String[] args) {
-		String[] g1 = {"Anne","Bjørn","Edvard","Fiona"};
+		String[] g1 = {"Anne","BjÃ¸rn","Edvard","Fiona"};
 		Facebook gr1= new Facebook("DAT102",g1);
-		System.out.println(gr1.leggTilVenner("Anne","Bjørn" ));
+		System.out.println(gr1.leggTilVenner("Anne","BjÃ¸rn" ));
 		
 		System.out.println(gr1.g.getNavn()[0]);
 		
 		gr1.leggTilVenner("Anne","Edvard" );
-		gr1.leggTilVenner("Bjørn","Edvard" );
-		gr1.leggTilVenner("Bjørn","Fiona" );
+		gr1.leggTilVenner("BjÃ¸rn","Edvard" );
+		gr1.leggTilVenner("BjÃ¸rn","Fiona" );
 		
-		gr1.visGraf();
-		
-		
-		
-		
+		gr1.visGraf();		
 	}
 
 }
